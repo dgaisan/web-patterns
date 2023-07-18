deccd5ee9b704296ed7b22b9853f6459c07d7874
dgaisan <dima.gaisan@gmail.com>
2023-07-18 05:44:19 +0300

    [basic-go-gin-server] Add GET and POST routes

--- basic-go-gin-server/main.go
+++ basic-go-gin-server/main.go
@@ -6,11 +6,38 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	Id        string
+	FirstName string
+	LastName  string
+	Email     string
+}
+
+var userMap = make(map[string]User)
+
 func createRouter() *gin.Engine {
 	router := gin.Default()
 
 	router.GET("/ping", func(context *gin.Context) {
 		context.String(http.StatusOK, "pong")
+	})
+
+	router.GET("/users", func(context *gin.Context) {
+		users := []string{}
+		for _, u := range userMap {
+			users = append(users, u.FirstName+" "+u.LastName)
+		}
+		context.JSON(http.StatusOK, users)
+	})
+
+	router.POST("user", func(context *gin.Context) {
+		var user User
+		if err := context.Bind(&user); err != nil {
+			context.String(http.StatusOK, "Error: Unable to create a User")
+		} else {
+			userMap[user.Id] = user
+			context.JSON(http.StatusCreated, gin.H{"user": user.FirstName + " " + user.LastName, "created": true})
+		}
 	})
 
 	return router
