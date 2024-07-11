4f4c8a743c407e46a05065d6af02efe6146db0af
dgaisan <dima.gaisan@gmail.com>
2024-07-11 15:55:52 +0300

    [basic-go-gin-server] fixing broken rest methods

--- basic-go-gin-server/main.go
+++ basic-go-gin-server/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/go-redis/redis/v8"
+
+	"log"
+
+	"os"
 )
 
 type User struct {
@@ -13,10 +22,30 @@ type User struct {
 	Email     string
 }
 
-var userMap = make(map[string]User)
+var redisClient *redis.Client
+var userMap map[string]User
+
+var ctx = context.Background()
 
 func createRouter() *gin.Engine {
 	router := gin.Default()
+	userMap = make(map[string]User)
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     strEnv("REDIS_HOST", "localhost:6379"),
+		Password: strEnv("REDIS_PASSWORD", ""),
+		DB:       intEnv("REDIS_DB", 0),
+	})
+
+	pong, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		fmt.Println("Error creating redis server")
+		log.Fatalf("Could not connect to Redis: %v", err)
+		panic(1)
+	}
+	fmt.Println("Connected to Redis:", pong)
+	log.Println("Connected to Redis:", pong)
+
+	// TODO: start caching values
 
 	router.GET("/ping", func(context *gin.Context) {
 		context.String(http.StatusOK, "pong")
@@ -43,7 +72,23 @@ func createRouter() *gin.Engine {
 	return router
 }
 
+func strEnv(key, fallback string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return fallback
+}
+
+func intEnv(key string, fallback int) int {
+	if val, ok := os.LookupEnv(key); ok {
+		if ret, err := strconv.Atoi(val); err != nil {
+			return ret
+		}
+	}
+	return fallback
+}
+
 func main() {
 	router := createRouter()
-	router.Run(":8001")
+	router.Run(strEnv("PORT", ":8001"))
 }
